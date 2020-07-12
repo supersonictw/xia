03066dd0629510e3a599a7fb3375c14abfefa5fc
SuperSonic <supersonic@livemail.tw>
2020-07-13 01:44:41 +0800

    Update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,21 +3,16 @@
     <h1>LINE Web Client for any platform</h1>
     <p>
       This is an
-      <a
-        href="https://github.com/supersonictw/xia"
-        target="_blank"
-        rel="noopener"
-        >OSS</a
-      >
+      <a href="https://github.com/supersonictw/xia" target="_blank" rel="noopener">OSS</a>
       with
       <a
         href="https://github.com/supersonictw/xia/blob/master/LICENSE"
         target="_blank"
         rel="noopener"
-        >Mozilla Public License v.2.0</a
-      >.
+      >Mozilla Public License v.2.0</a>.
     </p>
     <h3>Login with LINE account</h3>
+    <p>{{ loginStatus }}</p>
     <form class="login" method="post">
       <div>
         <label for="line-identity">Email:</label>
@@ -45,42 +40,156 @@
         <input type="submit" value="Login" @click="login" />
       </div>
     </form>
+    <p>IP: {{ user.ip_addr }}</p>
   </div>
 </template>
 
 <script>
 import Constant from "@/data/const.js";
 
-import Thrift from "@/computes/thrift.js";
-import TalkService from "@/computes/line/TalkService.js";
+import utf8 from "utf8";
+import thrift from "thrift";
+import crypto from "node-bignumber";
+
+import lineType from "@/computes/line/line_types.js";
+import talkService from "@/computes/line/TalkService.js";
 
 export default {
   name: "Login",
   data() {
     return {
       user: {
+        ip_addr: "Unknown",
         identity: "",
-        password: "",
+        password: ""
       },
+      loginStatus: ""
     };
   },
   methods: {
-    login(e) {
+    async login(e) {
       e.preventDefault();
-      console.log(this.user.identity, this.user.password);
-      let client = this.connect();
-      client.loginZ();
+      if (this.user.identity.length < 1 || this.user.password.length < 1) {
+        return;
+      }
+      const loginClient = this.connect(Constant.LINE_LOGIN_PATH);
+      const loginRequest = await this.getCredential();
+      const loginResponse = await loginClient
+        .loginZ(loginRequest)
+        .catch(error => (this.loginStatus = error.reason));
+      if (loginResponse !== this.loginStatus) {
+        await this.verifyPinCode(loginResponse);
+      }
     },
-    connect() {
-      const transport = new Thrift.TXHRTransport(
-        Constant.LINE_SERVER_HOST + Constant.LINE_QUERY_PATH
+    async getCredential() {
+      const authClient = this.connect(Constant.LINE_AUTH_PATH);
+      const rsaKey = await authClient.getRSAKeyInfo(
+        lineType.IdentityProvider.LINE
       );
-      const protocol = new Thrift.TBinaryProtocol(transport);
-      const client = new TalkService.TalkServiceClient(protocol);
-      transport.open();
-      return client;
+      const message = utf8.encode(
+        String.fromCharCode(rsaKey.sessionKey.length) +
+          rsaKey.sessionKey +
+          String.fromCharCode(this.user.identity.length) +
+          this.user.identity +
+          String.fromCharCode(this.user.password.length) +
+          this.user.password
+      );
+      const rsa = new crypto.Key();
+      rsa.setPublic(rsaKey.nvalue, rsaKey.evalue);
+      const encrypted_msg = rsa.encrypt(message).toString("hex");
+      return new lineType.LoginRequest({
+        type: lineType.LoginType.ID_CREDENTIAL,
+        identityProvider: lineType.IdentityProvider.LINE,
+        identifier: rsaKey.keynm,
+        password: encrypted_msg,
+        keepLoggedIn: true,
+        accessLocation: this.user.ip_addr,
+        systemName: Constant.NAME,
+        certificate: null,
+        e2eeVersion: 0
+      });
     },
+    async verifyPinCode(loginResult) {
+      this.loginStatus = `Confirm your PinCode with ${loginResult.pinCode} in 2 minutes.`;
+      switch (loginResult.type) {
+        case lineType.LoginResultType.REQUIRE_DEVICE_CONFIRM: {
+          const certificateResponse = await fetch(
+            `${Constant.LINE_USE_HTTPS ? "https" : "http"}://${
+              Constant.LINE_SERVER_HOST
+            }${Constant.LINE_CERTIFICATE_PATH}`,
+            {
+              method: "GET",
+              cache: "no-cache",
+              headers: {
+                Accept: "application/json",
+                "X-Line-Access": loginResult.verifier,
+                "X-Line-Application": Constant.LINE_APPLICATION_IDENTITY
+              },
+              keepalive: true
+            }
+          );
+          const accessKey = await certificateResponse.json();
+          const verifyClient = this.connect(Constant.LINE_LOGIN_PATH);
+          verifyClient.setHeader;
+          const verifyRequest = new lineType.LoginRequest({
+            type: lineType.LoginType.QRCODE,
+            identityProvider: lineType.IdentityProvider.LINE,
+            verifier: accessKey.result.verifier,
+            keepLoggedIn: true,
+            accessLocation: this.user.ip_addr,
+            systemName: Constant.NAME,
+            e2eeVersion: 0
+          });
+          const verifyResult = await verifyClient.loginZ(verifyRequest);
+          if (verifyResult.type == lineType.LoginResultType.SUCCESS) {
+            this.loginStatus = "Successful";
+            this.$store.commit("updateAuthToken", verifyResult.authToken);
+            return true;
+          }
+          this.loginStatus = "Unknown Error";
+          return false;
+        }
+        case lineType.LoginResultType.REQUIRE_QRCODE:
+          this.loginStatus = "QR Code Login Required. But nothing to do.";
+          return false;
+        case lineType.LoginResultType.SUCCESS:
+          this.loginStatus = "Successful";
+          this.$store.commit("updateAuthToken", loginResult.authToken);
+          return true;
+      }
+    },
+    async getUserIP() {
+      let response = await fetch("https://restapi.starinc.xyz/basic/ip");
+      let data = await response.json();
+      return data.data.ip_addr;
+    },
+    connect(path) {
+      let host = Constant.LINE_SERVER_HOST;
+      let port = Constant.LINE_USE_HTTPS ? 443 : 80;
+      let opts = {
+        transport: thrift.TBufferedTransport,
+        protocol: thrift.TCompactProtocol,
+        headers: {
+          "X-Line-Application": Constant.LINE_APPLICATION_IDENTITY
+        },
+        https: Constant.LINE_USE_HTTPS,
+        path: path,
+        useCORS: true
+      };
+
+      let connection = thrift.createHttpConnection(host, port, opts);
+      let thriftClient = thrift.createHttpClient(talkService, connection);
+
+      connection.on("error", err => {
+        console.error(err);
+      });
+
+      return thriftClient;
+    }
   },
+  async created() {
+    this.user.ip_addr = await this.getUserIP();
+  }
 };
 </script>
 
--- src/data/const.js
+++ src/data/const.js
@@ -1,5 +1,11 @@
 export default {
-  LINE_SERVER_HOST: "https://gf.line.naver.jp",
-  LINE_QUERY_PATH: "/S3",
-  LINE_POLL_PATH: "/P3",
+  NAME: "XIA",
+  LINE_USE_HTTPS: true,
+  LINE_APPLICATION_IDENTITY: "CHROMEOS\t2.3.8\tChrome_OS\txia",
+  LINE_SERVER_HOST: "gf.line.naver.jp",
+  LINE_CERTIFICATE_PATH: "/Q",
+  LINE_LOGIN_PATH: "/api/v4p/rs",
+  LINE_AUTH_PATH: "/api/v4/TalkService.do",
+  LINE_QUERY_PATH: "/S4",
+  LINE_POLL_PATH: "/P4",
 };
