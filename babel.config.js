1c990b81ca59ae2489368515d2e2297544d8e450
SuperSonic <supersonic@livemail.tw>
2020-07-19 17:39:23 +0800

    Update for Netlify Demo

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
+/*jshint esversion: 8 */
+/*
+    XIA - LINE Web Client
+    ---
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  (c) 2020 SuperSonic. (https://github.com/supersonictw)
+*/
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
-}
+};
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+/*jshint esversion: 8 */
+/*
+    XIA - LINE Web Client
+    ---
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  (c) 2020 SuperSonic. (https://github.com/supersonictw)
+*/
+
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/xia/' : '/',
     devServer: {
         disableHostCheck: true
     }
-}
+};
